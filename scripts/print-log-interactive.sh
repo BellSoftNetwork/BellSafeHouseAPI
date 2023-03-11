920e80b814acde3406c2d5ca134a3c3dee351bed
Jisu Yu <det3652000@naver.com>
2023-03-12 00:38:57 +0900

    Resolve "로컬 환경의 상태 및 로그를 한 번에 볼 수 있는 스크립트 필요"
    * Revert "refactor: 외부 사용이 용이하도록 APPLICATION_TYPE 변수 별도로 분리"
    
    This reverts commit 0f69b0e3e5ba35aaf2d19a37f066e7c9ceb791a5.
    
    * Revert "feat: 쿠버네티스에서 실행 중인 Pod의 로그를 확인할 수 있는 스크립트 작업"
    
    This reverts commit fd37cb5cfbebeb3ce75c1d792c88b155d7fbb08e.
    
    * Revert "feat: 쿠버네티스에서 실행 중인 Pod의 로그를 확인할 수 있는 Gradle task 추가"
    
    This reverts commit 56b152d4cc40a2acb5afac1e1f3d41a8a478c5b0.
    
    * Revert "fix: 인수에 컨테이너를 지정하지 않은 경우에도 정상 실행되도록 변경"
    
    This reverts commit b36962dc257e331e4cf97b6851ba1f6afa5d671e.
    
    * feat: 쿠버네티스에서 실행 중인 Pod의 로그를 확인할 수 있는 Interactive 스크립트 작업
    
    * fix: 인수에 컨테이너를 지정하지 않은 경우에도 정상 실행되도록 변경
    
    * feat: 쿠버네티스에서 실행 중인 Pod의 로그를 확인할 수 있는 Gradle task 추가
    
    * feat: 쿠버네티스에서 실행 중인 Pod의 로그를 확인할 수 있는 스크립트 작업
    
    * refactor: 외부 사용이 용이하도록 APPLICATION_TYPE 변수 별도로 분리

---
+++ scripts/print-log-interactive.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+source "${BASH_SOURCE%/*}/configs/base.sh"
+source "${BASH_SOURCE%/*}/configs/project.sh"
+source "${BASH_SOURCE%/*}/configs/deploy.sh"
+
+# Get app types
+APP_TYPE=$(${KUBECTL} -n "${PROJECT_NAMESPACE}" get -o custom-columns=appType:.metadata.labels.type pods | tail -n +2 | grep -v "<none>")
+
+# App type selection
+if [[ -n $APP_TYPE ]]; then
+  echo "로그를 출력할 Pod를 선택하세요."
+  select type in $APP_TYPE; do
+    SELECTED_APP_TYPE=$type
+    POD_NAME=$(${KUBECTL} -n "${PROJECT_NAMESPACE}" get -o name pods -l "type=$SELECTED_APP_TYPE" | head -n 1)
+    printf "\n"
+    break
+  done
+else
+  echo "Pod를 찾을 수 없습니다. 네임스페이스 ${PROJECT_NAMESPACE}를 확인하시거나 쿠버네티스 상태를 확인하십시오."
+  exit 1
+fi
+
+# Get container list
+CONTAINER_LIST_INIT=$(${KUBECTL} -n "${PROJECT_NAMESPACE}" get -o custom-columns=containerType:.spec.initContainers[*].name pods -l "type=$SELECTED_APP_TYPE" | tail -n +2 | grep -v "<none>")
+CONTAINER_LIST=$(${KUBECTL} -n "${PROJECT_NAMESPACE}" get -o custom-columns=containerType:.spec.containers[*].name pods -l "type=$SELECTED_APP_TYPE" | tail -n +2)
+
+# set CONTAINER_LIST_ALL to joined with init container, non-init container
+# if not exists, just set to $CONTAINER_LIST
+if [[ -n $CONTAINER_LIST_INIT ]]; then
+  CONTAINER_LIST_ALL=$(echo "$CONTAINER_LIST_INIT,$CONTAINER_LIST")
+else
+  CONTAINER_LIST_ALL=$CONTAINER_LIST
+fi
+
+# Container selection
+if [[ -n $CONTAINER_LIST_ALL ]]; then
+  OLD_IFS=$IFS
+  IFS=','
+  echo "로그를 출력 할 컨테이너를 선택하세요."
+  select container in $CONTAINER_LIST_ALL; do
+    SELECTED_CONTAINER=$container
+    IFS=$OLD_IFS
+    printf "\n"
+    break
+  done
+else
+  echo "Pod를 찾을 수 없습니다. 네임스페이스 ${PROJECT_NAMESPACE}를 확인하시거나 쿠버네티스 상태를 확인하십시오."
+  IFS=$OLD_IFS
+  exit 1
+fi
+
+# Print range selection
+echo "출력할 로그 라인 수를 입력하세요. (미입력시 follow 모드로 진입)"
+read LINE_TO_PRINT
+if [[ -n $LINE_TO_PRINT ]]; then
+  ${KUBECTL} -n "${PROJECT_NAMESPACE}" logs "${POD_NAME}" -c "${SELECTED_CONTAINER}" | tail -n "$LINE_TO_PRINT"
+  echo "로그 출력이 종료되었습니다."
+  exit 0
+else
+  # Follow
+  ${KUBECTL} -n "${PROJECT_NAMESPACE}" logs -f "${POD_NAME}" -c "${SELECTED_CONTAINER}"
+  echo "로그 출력이 종료되었습니다."
+  exit 0
+fi
